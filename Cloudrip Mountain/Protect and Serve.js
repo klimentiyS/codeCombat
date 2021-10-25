3ade6a94953917ce2f39eb01b8cb0d860efade74
Klimentiy <74042215+klimentiyS@users.noreply.github.com>
2021-10-25 20:16:06 +0600

    Create Protect and Serve.js

---
+++ Cloudrip Mountain/Protect and Serve.js
@@ -0,0 +1,33 @@
+const defends = [];
+defends[0] = {'x': 112, 'y': 57};
+defends[1] = {'x': 112, 'y': 52};
+defends[2] = {'x': 63, 'y': 79};
+defends[3] = {'x': 68, 'y': 79};
+defends[4] = {'x': 10, 'y': 67};
+defends[5] = {'x': 10, 'y': 62};
+defends[6] = {'x': 62, 'y': 5};
+defends[7] = {'x': 67, 'y': 5};
+
+let soldiers = [];
+let friends = hero.findFriends();
+for (let i = 0; i < friends.length; i++){
+    let friend = friends[i];
+    if (friend.type === "soldier"){
+        soldiers.push(friend);
+    }
+}
+
+while(true) {
+    for (let j = 0; j < soldiers.length; j++){
+        let soldier = soldiers[j];
+        let defend = defends[j];
+        let enemy = soldier.findNearestEnemy();
+        if (enemy){
+            hero.command(soldier, "defend", defend);
+        }
+        
+    }
+}
+
+
+
